8afffcdc381e30ab68dda0add7e54d4fd5da0e9c
Harman Singh <harman97it@gmail.com>
2023-04-12 22:35:57 +0100

    added a camera view  with button to take snapshot

--- exerciseTracker/MauiProgram.cs
+++ exerciseTracker/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace exerciseTracker;
+﻿using Camera.MAUI;
+
+namespace exerciseTracker;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCameraView()
 			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
@@ -14,7 +17,8 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		builder.Services.AddSingleton<MainViewModel>();
+
+        builder.Services.AddSingleton<MainViewModel>();
 
 		builder.Services.AddSingleton<MainPage>();
 
--- exerciseTracker/Views/CameraPage.xaml.cs
+++ exerciseTracker/Views/CameraPage.xaml.cs
@@ -2,9 +2,24 @@
 
 public partial class CameraPage : ContentPage
 {
-	public CameraPage(CameraViewModel viewModel)
+	public CameraPage()
 	{
 		InitializeComponent();
-		BindingContext = viewModel;
+	}
+
+	private void cameraView_CamerasLoaded(object sender, EventArgs e)
+	{
+		cameraView.Camera = cameraView.Cameras.First();
+
+		MainThread.BeginInvokeOnMainThread(async () =>
+		{
+			await cameraView.StopCameraAsync();
+			await cameraView.StartCameraAsync();
+		});
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		imageView.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.PNG);
 	}
 }
